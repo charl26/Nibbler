b35ab583140b1e5b9f7cff7d5d239ada9f0dc68b
chtheron <charltheron26@gmail.com>
2017-06-23 09:26:25 +0200

    changed colour of the background

--- dynamicLibraries/GLFW/glfw.cpp
+++ dynamicLibraries/GLFW/glfw.cpp
@@ -5,11 +5,8 @@
 #include "../dylib.hpp"
 #include "glfw.hpp"
 
-struct snakeHead {
-
-};
-
 GLFWwindow *window;
+
 gameControl::gameControl(CoreGame *ref) {
 	ScreenH = ref->getWindowHeight();
 	ScreenW = ref->getWindowWidth();
@@ -32,27 +29,83 @@ gameControl::~gameControl() {
 	glfwTerminate();
 }
 
-void processInput(GLFWwindow *window)
-{
-	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void gameControl::processInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+		switch (coreGame->getPlayer()[0]->getDirection()) {
+			case LEFT: {
+				coreGame->getPlayer()[0]->setDirection(DOWN);
+				break;
+			}
+			case RIGHT: {
+				coreGame->getPlayer()[0]->setDirection(UP);
+				break;
+			}
+			case UP: {
+				coreGame->getPlayer()[0]->setDirection(LEFT);
+				break;
+			}
+			case DOWN: {
+				coreGame->getPlayer()[0]->setDirection(RIGHT);
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+		switch (coreGame->getPlayer()[0]->getDirection()) {
+			case LEFT: {
+				coreGame->getPlayer()[0]->setDirection(UP);
+				break;
+			}
+			case RIGHT: {
+				coreGame->getPlayer()[0]->setDirection(DOWN);
+				break;
+			}
+			case UP: {
+				coreGame->getPlayer()[0]->setDirection(RIGHT);
+				break;
+			}
+			case DOWN: {
+				coreGame->getPlayer()[0]->setDirection(LEFT);
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
 
 void gameControl::draw() {
-	if(!glfwWindowShouldClose(window)) {
+	if (!glfwWindowShouldClose(window)) {
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		processInput(window);
 		glfwGetFramebufferSize(window, &this->ScreenW, &this->ScreenH);
 		glViewport(0, 0, this->ScreenW, this->ScreenH);
 		glfwSwapBuffers(window);
 		glfwSwapInterval(1);
+		processInput(window);
 		glfwPollEvents();
+	} else {
+		glfwTerminate();
+		exit(0);
 	}
 }
 
 void gameControl::update() {
 
 }
+
 int gameControl::getScreenW() { return ScreenW; }
 
 void gameControl::setScreenW(int ScreenW) { gameControl::ScreenW = ScreenW; }
@@ -60,7 +113,6 @@ void gameControl::setScreenW(int ScreenW) { gameControl::ScreenW = ScreenW; }
 int gameControl::getScreenH() { return ScreenH; }
 
 void gameControl::setScreenH(int ScreenH) { gameControl::ScreenH = ScreenH; }
-
 
 extern "C" gameControl *create(CoreGame *ref) { return new gameControl(ref); }
 
